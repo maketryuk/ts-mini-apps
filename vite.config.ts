23f8fd94b66805277b1f5724e248116c742469c8
maketryuk <langstons.pr@gmail.com>
2024-09-06 21:10:22 +0300

    Change package name and another changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import {defineConfig} from "vite";
-import {resolve} from "path";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import pkg from './package.json'
+import { log } from "console";
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'tg-mini-apps',
-            fileName: 'tg-mini-apps'
+            name: '@maketryuk/tg-mini-apps',
+            fileName: (format, name) => {
+                if (format === "es") {
+                    return `${name}.js`;
+                }
+
+                if (format === "umd") {
+                    return `${name}.cjs`;
+                }
+
+                return `${name}.${format}`;
+            },
         }
     }
 });
